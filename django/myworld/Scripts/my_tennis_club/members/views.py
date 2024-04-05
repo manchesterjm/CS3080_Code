5a9279a17091befd2e12919978a9ae456d7c5eb2
manchesterjm <manchesterjm@gmail.com>
2024-04-05 13:38:00 -0600

    Django
    Django project learning in class

---
+++ django/myworld/Scripts/my_tennis_club/members/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import loader
+from django.http import HttpResponse
+
+def members(request) :
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
